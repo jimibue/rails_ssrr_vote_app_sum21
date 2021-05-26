41412d607c36aa5980edf264a92d368e7774fabe
James Yeates <jimibue@gmail.com>
2021-05-26 09:56:02 -0600

    setup steps

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+categories = ['food', 'travel', 'music']
+
+10.times do |i|
+  Item.create(
+      name: Faker::Hacker.abbreviation,
+      description: Faker::Hacker.say_something_smart,
+      category: categories.sample,
+      likes: i
+  )
+end
+
+puts "Seeded: #{Item.all.size}"
